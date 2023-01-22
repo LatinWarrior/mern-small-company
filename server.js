503843a3d91906756899d079fe341a4e4e1a4bfe
Latin Warrior <ninjaguy@zoho.com>
2023-01-22 09:33:52 -0600

    Add errorHandler

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3500
+const express = require('express');
+const app = express();
+const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(logger);
 
-app.use('/', require('./routes/root'))
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Middleware.
+// This would work too.
+// app.use(express.static('public'))
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
 
 app.all('*', (req, res) => {
-    res.status(404)
+    res.status(404);
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
+        res.json({ message: '404 Not Found' });
     } else {
-        res.type('txt').send('404 Not Found')
+        res.type('txt').send('404 Not Found');
     }
-})
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+console.log(`I am past the app.all in server.js`);
+
+app.use(errorHandler);
+
+console.log(`I am past the errorHandler in server.js`);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
